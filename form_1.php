a4b8057fb305f993afa4654068f71201953dd76c
unknown <keithharolddavid01@gmail.com>
2024-01-22 19:21:44 -0800

    New File

---
+++ form_1.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+welcome <?php echo $_POST["firstname"] , $_POST["LASTNAME"]; ?<><BR>
+
+Your email address is: <?echp $_POST["email"]; ?><br>
+Your address is: <?php echo $_POST["address"]; ?>
+
+</body>
+</html>
